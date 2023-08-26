1e68d741259869ccf991306731523df566bebb97
raghib-shahriar <shahraghib.ece16@gmail.com>
2023-08-26 18:46:12 +0600

    minor adjustments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
             </div>
 
             <p className="text-xl mt-20">
-            Besides being a tech enthusiast, I am into movies and music. I love participating in cultural activities. Thoughout my college and university life, I have engaged in various bands and organizations as a musician. I have also organized a number of events in my univesity life, which boosted my teamwork skills and leadership competency.
+            As an engineering student, I always feel thrilled to solve critical problems in a strategic manner. Adapting to new programming languages and utility tools has been effortless, as I learned programming languages like C,C++,Java,Python,Matlab and a numerous number of utility tools through my University course curriculum. 
             </p>
 
             <br />
 
             <p className="text-xl">
-            As an engineering student, I always feel thrilled to solve critical problems in a strategic manner. Adapting to new programming languages and utility tools has been effortless, as I learned so many programming languages like C,C++,Java,Python,Matlab and a numerous number of utility tools through my University course curriculum.  
+            Besides being a tech enthusiast, I am into movies and music. I love participating in cultural activities. Thoughout my college and university life, I have engaged in various bands and organizations as a musician. I have also organized a number of events in my univesity life, which boosted my teamwork skills and leadership competency. 
             </p>
         </div>
     </div>
--- src/components/PortfolioR.jsx
+++ src/components/PortfolioR.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import fruitsmb from "../assets/portfolio/fruitsmb.png";
 import fruitspc from "../assets/portfolio/fruitspc.png";
-import gamingMb from "../assets/portfolio/gamingMb.png";
-import gamingPc from "../assets/portfolio/gamingPc.png";
+import summerpc from "../assets/portfolio/summer-sale-pc.png";
+import summermb from "../assets/portfolio/summer-sale-mb.png";
+
 
 const PortfolioR = () => {
   return (
@@ -13,6 +14,32 @@ const PortfolioR = () => {
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
                 <p className="py-6">Checkout some of my work right here</p>
             </div>
+      <div className='flex flex-col'>
+        <div className="flex items-center pb-8">
+          <p className="text-3xl font-bold inline">Summer Sale</p>
+          <button className="text-white bg-gradient-to-b mx-10 from-cyan-500 to-blue-500 px-6 py-3 rounded-md hover:scale-110 duration-300">
+              <a href="https://steady-buttercream-9833b7.netlify.app/" target="_blank">View Page</a>
+          </button>
+        </div>
+        
+      {/* pc Summer Sale */}
+        <div className='hidden relative h-[450px] w-[900px] lg:block'>
+          <div class="absolute top-0 right-0  overflow-hidden  w-[700px] h-[300px] rounded-lg border-2 border-black">
+            <img className='w-full animate-scrolling' src={summerpc} alt="" />
+          </div>
+          <div class="absolute bottom-0 left-0 overflow-hidden rounded-lg  w-[250px] h-[350px] border-2 border-black">
+              <img className='w-full animate-scrolling' src={summermb} alt="" />
+          </div>
+        </div>
+        {/* mobile gaming zone */}
+        <div className='flex flex-col justify-center items center lg:hidden'>
+            <div class="overflow-hidden rounded-lg  w-full h-[450px] border-2 border-black">
+                <img className='w-full animate-scrolling' src={summermb} alt="" />
+            </div>
+        </div>
+      </div>
+      <br />
+      <br />
       <div className='flex flex-col'>
         <div className="flex items-center pb-8">
           <p className="text-3xl font-bold inline">Fruit Burst</p>
@@ -33,33 +60,6 @@ const PortfolioR = () => {
         <div className='flex flex-col justify-center items center lg:hidden'>
             <div class="overflow-hidden rounded-lg  w-full h-[450px] border-2 border-black">
                 <img className='w-full animate-scrolling' src={fruitsmb} alt="" />
-            </div>
-        </div>
-      </div>
-      <br />
-      <br />
-
-      <div className='flex flex-col'>
-        <div className="flex items-center pb-8">
-          <p className="text-3xl font-bold inline">Gaming zone</p>
-          <button className="text-white bg-gradient-to-b mx-10 from-cyan-500 to-blue-500 px-6 py-3 rounded-md hover:scale-110 duration-300">
-              <a href="https://raghibku.github.io/gamer-zone/" target="_blank">View Page</a>
-          </button>
-        </div>
-        
-      {/* pc gamingZone */}
-        <div className='hidden relative h-[450px] w-[900px] lg:block'>
-          <div class="absolute top-0 right-0  overflow-hidden  w-[700px] h-[300px] rounded-lg border-2 border-black">
-            <img className='w-full animate-scrolling' src={gamingPc} alt="" />
-          </div>
-          <div class="absolute bottom-0 left-0 overflow-hidden rounded-lg  w-[250px] h-[350px] border-2 border-black">
-              <img className='w-full animate-scrolling' src={gamingMb} alt="" />
-          </div>
-        </div>
-        {/* mobile gaming zone */}
-        <div className='flex flex-col justify-center items center lg:hidden'>
-            <div class="overflow-hidden rounded-lg  w-full h-[450px] border-2 border-black">
-                <img className='w-full animate-scrolling' src={gamingMb} alt="" />
             </div>
         </div>
       </div>
